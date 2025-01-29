170ff210c63cbefa51274251afb3d895514a9d77
Alejandro <149619758+AlejandroAlvarezCastro@users.noreply.github.com>
2025-01-29 20:33:49 +0100

    Update label_10K.py

--- MULTIMODAL/TEXT/label_data/label_10K.py
+++ MULTIMODAL/TEXT/label_data/label_10K.py
@@ -2,7 +2,7 @@ from Labeler import Labeler
 
 # Classify 10K categories
 labeler_10K = Labeler(
-    root_directory='/home/aacastro/mchai/companies/',
+    root_directory='/home/aacastro/mchai/annotated/',
     output_file='/home/aacastro/Alejandro/ACA_MultichanelAI_2025/src/TEXT/label_data/labeled_gold_sample_10K.csv',
     mode='10K'
 )
