71a2afa7031ecc7eef0318de6c0427c83b5483cb
Alejandro <alejandro.alvrz.castro@gmail.com>
2025-01-29 20:29:45 +0100

    General class to label data

---
+++ MULTIMODAL/TEXT/label_data/label_10K.py
@@ -0,0 +1,9 @@
+from Labeler import Labeler
+
+# Classify 10K categories
+labeler_10K = Labeler(
+    root_directory='/home/aacastro/mchai/companies/',
+    output_file='/home/aacastro/Alejandro/ACA_MultichanelAI_2025/src/TEXT/label_data/labeled_gold_sample_10K.csv',
+    mode='10K'
+)
+labeler_10K.run()
