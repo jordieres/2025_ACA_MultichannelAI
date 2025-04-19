1f32f512205d77002b427c664bf754c9baa6c0cb
Alejandro Álvarez Castro <alejandro.alvrz.castro@gmail.com>
2025-04-19 16:10:28 +0200

    Definitive simulations file

--- ModelsResults/Simulations.py
+++ ModelsResults/Simulations.py
@@ -9,10 +9,11 @@ from ModelsResults.Answered.AnsweredSimulator import AnsweredSimulator
 results_df = pd.DataFrame(columns=["model_name", "elapsed_time", "accuracy", "precision", "recall", "f1_score"])
 
 LLM_MODELS = [
-        # 'deepseek-r1:1.5b', 'deepseek-r1:7b', 'deepseek-r1:8b', 'deepseek-r1:14b', #'deepseek-r1:32b',
+        'deepseek-r1:1.5b', 'deepseek-r1:7b', 'deepseek-r1:8b', 'deepseek-r1:14b', #'deepseek-r1:32b',
         'llama3:8b', 
         'llama3.1:8b', 
-        'llama3.2:1b', 'llama3.2:3b',
+        'llama3.2:1b', 
+        'llama3.2:3b',
         'phi3:3.8b', 'phi3:14b',
         'phi4',
         'qwen2:0.5b', 'qwen2:1.5b', 'qwen2:7b',
@@ -34,17 +35,17 @@ def main():
     monologue_results_path = "/home/aacastro/Alejandro/ACA_MultichanelAI_2025/clean_src/ModelsResults/Monologues/results/"
     answered_results_path = "/home/aacastro/Alejandro/ACA_MultichanelAI_2025/2025_ACA_MultichannelAI/ModelsResults/Answered/results"
 
-    # print("Running QA simulation...")
-    # qa_sim = QASimulator(models=LLM_MODELS, labeled_data_path=qa_labeled_data, results_path=qa_results_path, results_df=results_df)
-    # qa_sim.run()
+    print("Running QA simulation...")
+    qa_sim = QASimulator(models=LLM_MODELS, labeled_data_path=qa_labeled_data, results_path=qa_results_path, results_df=results_df)
+    qa_sim.run()
 
-    # print("Running 10-K simulation...")
-    # k10_sim = TenKSimulator(models=LLM_MODELS, labeled_data_path=k10_labeled_data, results_path=k10_results_path, results_df=results_df)
-    # k10_sim.run()
+    print("Running 10-K simulation...")
+    k10_sim = TenKSimulator(models=LLM_MODELS, labeled_data_path=k10_labeled_data, results_path=k10_results_path, results_df=results_df)
+    k10_sim.run()
 
-    # print("Running Monologue simulation...")
-    # monologue_sim = MonologueSimulator(models=LLM_MODELS, labeled_data_path=monologues_labeled_data, results_path=monologue_results_path, results_df=results_df)
-    # monologue_sim.run()
+    print("Running Monologue simulation...")
+    monologue_sim = MonologueSimulator(models=LLM_MODELS, labeled_data_path=monologues_labeled_data, results_path=monologue_results_path, results_df=results_df)
+    monologue_sim.run()
 
     print("Running Answered simulation...")
     monologue_sim = AnsweredSimulator(models=LLM_MODELS, labeled_data_path=answered_labeled_data, results_path=answered_results_path, results_df=results_df)
